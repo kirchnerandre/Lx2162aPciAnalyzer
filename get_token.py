fcb81cb0a57b05fd32f95a3dadd61213294e56fb
Andre Kirchner <kirchnerandre@hotmail.com>
2026-08-26 16:32:04 -0700

    Get token

---
+++ get_token.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+import azure.identity
+import azure.kusto.data
+
+
+_cluster = "https://ingest-kvc-g17c54juuue55kc9ay.southcentralus.kusto.windows.net"
+
+
+def main():
+    credential = azure.identity.DeviceCodeCredential()
+
+    token = credential.get_token("https://kusto.kusto.windows.net/.default")
+
+    print()
+    print("ACCESS TOKEN:")
+    print(token.token)
+    print()
+
+
+if __name__ == "__main__":
+    main()
