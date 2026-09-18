2dcd9d5075da8bfa562251a49e71b7f61aaee2e4
Andre Kirchner <kirchnerandre@hotmail.com>
2026-09-18 09:34:23 -0700

    Do not own device

--- kernel/Lx2162aPciAnalyzer.c
+++ kernel/Lx2162aPciAnalyzer.c
@@ -1,355 +1,228 @@
 
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/pci.h>
-#include <linux/fs.h>
-#include <linux/cdev.h>
-#include <linux/device.h>
-#include <linux/uaccess.h>
 #include <linux/io.h>
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
 #include <linux/slab.h>
 
 
-#define DRIVER_NAME             "Lx2162aPciAnalyzer"
-#define DEVICE_NAME             "Lx2162aPciAnalyzer"
+#define DRIVER_NAME       "Lx2162aPciAnalyzer"
+#define PCI_VENDOR_ID_MS  0x1414
+#define PCI_DEVICE_ID_MS  0x00b9
 
 
-#define LX2162A_PCI_VENDOR_ID   0x1234
-#define LX2162A_PCI_DEVICE_ID   0x5678
-#define LX2162A_PCI_BAR         0
-#define LX2162A_PCI_MAGIC       'M'
+
+static int bar = 0;
+module_param(bar, int, 0444);
+MODULE_PARM_DESC(bar, "PCI BAR number to map (default: 0)");
 
 
-#define BAR_REGISTER_READ       _IOWR(LX2162A_PCI_MAGIC, 0, struct bar_register)
-#define BAR_REGISTER_WRITE      _IOW (LX2162A_PCI_MAGIC, 1, struct bar_register)
-
-
-struct bar_register
+struct lx2162a_bar
 {
-    __u32 offset;
-    __u32 value;
+    struct pci_dev* pdev;
+    void __iomem* base;
+    resource_size_t size;
 };
 
 
-struct lx2162a_pci_device
+static struct lx2162a_bar* devdata;
+
+
+static ssize_t lx2162a_read(struct file* file, char __user* buf, size_t count, loff_t* ppos)
 {
-    struct pci_dev*     PDev;
-    void __iomem*       Bar;
-    resource_size_t     BarStart;
-    resource_size_t     BarSize;
-    dev_t               DevT;
-    struct cdev         CDev;
-    struct device*      Device;
-};
+    u32 value;
+    resource_size_t offset = *ppos;
 
-
-static struct class* lx2162a_pci_class;
-
-
-static int lx2162a_pci_analyzer_open(struct inode* INode, struct file* File)
-{
-    struct lx2162a_pci_device* device;
-
-    device = container_of(
-        INode->i_cdev,
-        struct lx2162a_pci_device,
-        CDev);
-
-    File->private_data = device;
-
-    return 0;
-}
-
-
-static int lx2162a_pci_analyzer_release(struct inode* INode, struct file* File)
-{
-    return 0;
-}
-
-
-static long lx2162a_pci_analyzer_ioctl(struct file* File, unsigned int Command, unsigned long Arg)
-{
-    struct lx2162a_pci_device*  device          = File->private_data;
-    struct bar_register         bar_register;
-
-    if (!device)
+    if (count != sizeof(value))
     {
+        return -EINVAL;
+    }
+
+    if (offset & 0x3)
+    {
+        return -EINVAL;
+    }
+
+    if (offset + sizeof(value) > devdata->size)
+    {
+        return -EINVAL;
+    }
+
+    value = ioread32(devdata->base + offset);
+
+    if (copy_to_user(buf, &value, sizeof(value)))
+    {
+        return -EFAULT;
+    }
+
+    *ppos += sizeof(value);
+
+    return sizeof(value);
+}
+
+
+static ssize_t lx2162a_write(struct file* file, const char __user* buf, size_t count, loff_t* ppos)
+{
+    u32 value;
+    resource_size_t offset = *ppos;
+
+    if (count != sizeof(value))
+    {
+        return -EINVAL;
+    }
+
+    if (offset & 0x3)
+    {
+        return -EINVAL;
+    }
+
+    if (offset + sizeof(value) > devdata->size)
+    {
+        return -EINVAL;
+    }
+
+    if (copy_from_user(&value, buf, sizeof(value)))
+    {
+        return -EFAULT;
+    }
+
+    iowrite32(value, devdata->base + offset);
+
+    *ppos += sizeof(value);
+
+    return sizeof(value);
+}
+
+
+static const struct file_operations lx2162a_fops =
+{
+    .owner  = THIS_MODULE,
+    .read   = lx2162a_read,
+    .write  = lx2162a_write,
+    .llseek = default_llseek,
+};
+
+
+static struct miscdevice lx2162a_miscdev =
+{
+    .minor  = MISC_DYNAMIC_MINOR,
+    .name   = "lx2162a_pci_bar",
+    .fops   = &lx2162a_fops,
+    .mode   = 0600,
+};
+
+
+static int __init lx2162a_init(void)
+{
+    struct pci_dev* pdev;
+    unsigned long flags;
+    int ret;
+
+    pr_info(DRIVER_NAME ": initializing\n");
+
+    if (bar < 0 || bar >= PCI_STD_NUM_BARS)
+    {
+        pr_err(DRIVER_NAME ": invalid BAR number %d\n", bar);
+        return -EINVAL;
+    }
+
+    pdev = pci_get_device(PCI_VENDOR_ID_MS, PCI_DEVICE_ID_MS, NULL);
+
+    if (!pdev)
+    {
+        pr_err(DRIVER_NAME ": PCI device %04x:%04x not found\n", PCI_VENDOR_ID_MS, PCI_DEVICE_ID_MS);
         return -ENODEV;
     }
 
-    switch (Command)
+    devdata = kzalloc(sizeof(*devdata), GFP_KERNEL);
+
+    if (!devdata)
     {
-    case BAR_REGISTER_READ:
-        if (copy_from_user(&bar_register, (void __user*)Arg, sizeof(bar_register)))
-        {
-            return -EFAULT;
-        }
-
-        if (bar_register.offset & 0x3)
-        {
-            return -EINVAL;
-        }
-
-        if ((resource_size_t)bar_register.offset + sizeof(u32) > device->BarSize)
-        {
-            return -EINVAL;
-        }
-
-        bar_register.value = ioread32(device->Bar + bar_register.offset);
-
-        if (copy_to_user((void __user*)Arg, &bar_register, sizeof(bar_register)))
-        {
-            return -EFAULT;
-        }
-
-        return 0;
-
-    case BAR_REGISTER_WRITE:
-        if (copy_from_user(&bar_register, (void __user*)Arg, sizeof(bar_register)))
-        {
-            return -EFAULT;
-        }
-
-        if (bar_register.offset & 0x3)
-        {
-            return -EINVAL;
-        }
-
-        if ((resource_size_t)bar_register.offset + sizeof(u32) > device->BarSize)
-        {
-            return -EINVAL;
-        }
-
-        iowrite32(bar_register.value, device->Bar + bar_register.offset);
-
-        readl(device->Bar + bar_register.offset);
-
-        return 0;
-
-    default:
-        return -ENOTTY;
-    }
-}
-
-
-static const struct file_operations lx2162a_pci_fops =
-{
-    .owner              = THIS_MODULE,
-    .open               = lx2162a_pci_analyzer_open,
-    .release            = lx2162a_pci_analyzer_release,
-    .unlocked_ioctl     = lx2162a_pci_analyzer_ioctl,
-};
-
-
-static int lx2162a_pci_analyzer_probe(struct pci_dev* PDev, const struct pci_device_id* Id)
-{
-    struct lx2162a_pci_device*  device;
-    int                         retval;
-
-    dev_info(&PDev->dev, "Lx2162aPciAnalyzer: device found: %04x:%04x\n", PDev->vendor, PDev->device);
-
-    retval = pci_enable_device(PDev);
-
-    if (retval)
-    {
-        dev_err(&PDev->dev, "Lx2162aPciAnalyzer: pci_enable_device() failed: %d\n", retval);
-        return retval;
+        pci_dev_put(pdev);
+        return -ENOMEM;
     }
 
-    retval = pci_request_region(PDev, LX2162A_PCI_BAR, DRIVER_NAME);
+    devdata->pdev = pdev;
 
-    if (retval)
+    pr_info(DRIVER_NAME ": found %04x:%04x at %s\n", pdev->vendor, pdev->device, pci_name(pdev));
+
+    flags           = pci_resource_flags(pdev, bar);
+    devdata->size   = pci_resource_len(pdev, bar);
+
+    pr_info(DRIVER_NAME ": BAR%d start=0x%llx size=0x%llx flags=0x%lx\n", bar, (unsigned long long)pci_resource_start(pdev, bar), (unsigned long long)devdata->size, flags);
+
+    if (!(flags & (IORESOURCE_MEM | IORESOURCE_IO)))
     {
-        dev_err(&PDev->dev, "Lx2162aPciAnalyzer: pci_request_region() failed: %d\n", retval);
-
-        pci_disable_device(PDev);
-        return retval;
+        pr_err(DRIVER_NAME ": BAR%d is not an I/O or memory resource\n", bar);
+        ret = -EINVAL;
+        goto err_put;
     }
 
-    device = kzalloc(sizeof(*device), GFP_KERNEL);
-
-    if (!device)
+    if (!devdata->size)
     {
-        retval = -ENOMEM;
-        goto err_release_region;
+        pr_err(DRIVER_NAME ": BAR%d has zero length\n", bar);
+        ret = -EINVAL;
+        goto err_put;
     }
 
-    device->PDev = PDev;
+    devdata->base = pci_iomap(pdev, bar, 0);
 
-    device->BarStart    = pci_resource_start(PDev, LX2162A_PCI_BAR);
-    device->BarSize     = pci_resource_len  (PDev, LX2162A_PCI_BAR);
-
-    dev_info(&PDev->dev, "Lx2162aPciAnalyzer: BAR%d start = 0x%llx\n", LX2162A_PCI_BAR, (unsigned long long)device->BarStart);
-
-    dev_info(&PDev->dev, "Lx2162aPciAnalyzer: BAR%d size  = 0x%llx (%llu bytes)\n", LX2162A_PCI_BAR, (unsigned long long)device->BarSize, (unsigned long long)device->BarSize);
-
-    if (!(pci_resource_flags(PDev, LX2162A_PCI_BAR) & IORESOURCE_MEM))
+    if (!devdata->base)
     {
-        dev_err(&PDev->dev, "Lx2162aPciAnalyzer: BAR%d is not a memory BAR\n", LX2162A_PCI_BAR);
-
-        retval = -ENODEV;
-        goto err_free;
+        pr_err(DRIVER_NAME ": pci_iomap(BAR%d) failed\n", bar);
+        ret = -ENOMEM;
+        goto err_put;
     }
 
-    device->Bar = pci_iomap(PDev, LX2162A_PCI_BAR, 0);
+    ret = misc_register(&lx2162a_miscdev);
 
-    if (!device->Bar)
+    if (ret)
     {
-        dev_err(&PDev->dev, "Lx2162aPciAnalyzer: pci_iomap() failed\n");
-
-        retval = -ENOMEM;
-        goto err_free;
+        pr_err(DRIVER_NAME ": misc_register failed: %d\n", ret);
+        pci_iounmap(pdev, devdata->base);
+        goto err_put;
     }
 
-    dev_info(&PDev->dev, "Lx2162aPciAnalyzer: BAR%d mapped at %p\n", LX2162A_PCI_BAR, device->Bar);
-
-    retval = alloc_chrdev_region(&device->DevT, 0, 1, DEVICE_NAME);
-
-    if (retval)
-    {
-        dev_err(&PDev->dev, "Lx2162aPciAnalyzer: alloc_chrdev_region() failed: %d\n", retval);
-        goto err_unmap;
-    }
-
-    cdev_init(&device->CDev, &lx2162a_pci_fops);
-
-    device->CDev.owner = THIS_MODULE;
-
-    retval = cdev_add(&device->CDev, device->DevT, 1);
-
-    if (retval)
-    {
-        dev_err(&PDev->dev, "Lx2162aPciAnalyzer: cdev_add() failed: %d\n", retval);
-        goto err_unregister;
-    }
-
-    device->Device = device_create(lx2162a_pci_class, &PDev->dev, device->DevT, device, DEVICE_NAME);
-
-    if (IS_ERR(device->Device))
-    {
-        retval = PTR_ERR(device->Device);
-
-        dev_err(&PDev->dev, "Lx2162aPciAnalyzer: device_create() failed: %d\n", retval);
-
-        goto err_cdev;
-    }
-
-    pci_set_drvdata(PDev, device);
-
-    dev_info(&PDev->dev, "Lx2162aPciAnalyzer driver loaded successfully\n");
+    pr_info(DRIVER_NAME ": BAR%d mapped, device available as /dev/%s\n", bar, lx2162a_miscdev.name);
 
     return 0;
 
-err_cdev:
-    cdev_del(&device->CDev);
-
-err_unregister:
-    unregister_chrdev_region(device->DevT, 1);
-
-err_unmap:
-    pci_iounmap(PDev, device->Bar);
-
-err_free:
-    kfree(device);
-
-err_release_region:
-    pci_release_region(PDev, LX2162A_PCI_BAR);
-    pci_disable_device(PDev);
-
-    return retval;
+err_put:
+    kfree(devdata);
+    devdata = NULL;
+    pci_dev_put(pdev);
+    return ret;
 }
 
 
-static void lx2162a_pci_analyzer_remove(struct pci_dev* PDev)
+static void __exit lx2162a_exit(void)
 {
-    struct lx2162a_pci_device* device;
-
-    device = pci_get_drvdata(PDev);
-
-    if (!device)
+    if (!devdata)
     {
         return;
     }
 
-    dev_info(&PDev->dev, "Lx2162aPciAnalyzer: removing Lx2162aPciAnalyzer driver\n");
+    misc_deregister(&lx2162a_miscdev);
 
-    device_destroy(lx2162a_pci_class, device->DevT);
-
-    cdev_del(&device->CDev);
-
-    unregister_chrdev_region(device->DevT, 1);
-
-    if (device->Bar)
+    if (devdata->base)
     {
-        pci_iounmap(PDev, device->Bar);
+        pci_iounmap(devdata->pdev, devdata->base);
     }
 
-    pci_release_region(PDev, LX2162A_PCI_BAR);
+    pci_dev_put(devdata->pdev);
 
-    pci_disable_device(PDev);
+    kfree(devdata);
+    devdata = NULL;
 
-    kfree(device);
+    pr_info(DRIVER_NAME ": unloaded\n");
 }
 
 
-static const struct pci_device_id lx2162a_pci_ids[] = {
-    {
-        PCI_DEVICE(LX2162A_PCI_VENDOR_ID, LX2162A_PCI_DEVICE_ID)
-    },
-
-    { 0, }
-};
-
-
-MODULE_DEVICE_TABLE(pci, lx2162a_pci_ids);
-
-
-static struct pci_driver lx2162a_pci_driver =
-{
-    .name       = DRIVER_NAME,
-    .id_table   = lx2162a_pci_ids,
-
-    .probe      = lx2162a_pci_analyzer_probe,
-    .remove     = lx2162a_pci_analyzer_remove,
-};
-
-
-static int __init lx2162a_pci_analyzer_init(void)
-{
-    int retval;
-
-    lx2162a_pci_class = class_create(DRIVER_NAME);
-
-    if (IS_ERR(lx2162a_pci_class))
-    {
-        return PTR_ERR(lx2162a_pci_class);
-    }
-
-    retval = pci_register_driver(&lx2162a_pci_driver);
-
-    if (retval)
-    {
-        class_destroy(lx2162a_pci_class);
-        return retval;
-    }
-
-    pr_info("Lx2162aPciAnalyzer: driver loaded\n");
-
-    return 0;
-}
-
-
-static void __exit lx2162a_pci_analyzer_exit(void)
-{
-    pci_unregister_driver(&lx2162a_pci_driver);
-
-    class_destroy(lx2162a_pci_class);
-
-    pr_info("Lx2162aPciAnalyzer: driver unloaded\n");
-}
-
-
-module_init(lx2162a_pci_analyzer_init);
-module_exit(lx2162a_pci_analyzer_exit);
+module_init(lx2162a_init);
+module_exit(lx2162a_exit);
 
 
 MODULE_LICENSE("GPL");
