291d94feb30041693da0888d1ec636ba76be8c57
Andre Kirchner <kirchnerandre@hotmail.com>
2026-09-25 21:52:42 +0000

    Set Microsoft PCI vendor ID

--- tests/D3Good/pci_d3_good.h
+++ tests/D3Good/pci_d3_good.h
@@ -7,12 +7,12 @@
 #include "hw/pci/pci_device.h"
 
 #define VIRTUAL_PCI_DEVICE_CAPABILITIES_OFFSET  0x040
-#define VIRTUAL_PCI_DEVICE_VENDOR_ID            0x1b36
-#define VIRTUAL_PCI_DEVICE_DEVICE_ID            0x1100
-#define VIRTUAL_PCI_DEVICE_REVISION             0x02
+#define VIRTUAL_PCI_DEVICE_VENDOR_ID            0x1414
+#define VIRTUAL_PCI_DEVICE_DEVICE_ID            0x00b9
+#define VIRTUAL_PCI_DEVICE_REVISION             0x01
 
-#define TYPE_VIRTUAL_PCI_DEVICE                 "pci_d3_good"
-#define DESC_VIRTUAL_PCI_DEVICE                 "pci_d3_good"
+#define TYPE_VIRTUAL_PCI_DEVICE                 "lx2162a_pci_device"
+#define DESC_VIRTUAL_PCI_DEVICE                 "lx2162a_pci_device"
 
 OBJECT_DECLARE_TYPE(VirtualPciDevice, VirtualPciDeviceClass, VIRTUAL_PCI_DEVICE);
 
